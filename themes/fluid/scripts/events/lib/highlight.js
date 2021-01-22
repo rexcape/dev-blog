95a7ef400aa5a0c9c4ceef4b337904dde545c477
mazixiang <mazixiang2012@gmail.com>
2021-01-22 10:52:59 +0800

    update theme

--- themes/fluid/scripts/events/lib/highlight.js
+++ themes/fluid/scripts/events/lib/highlight.js
@@ -16,21 +16,40 @@ module.exports = (hexo) => {
     hexo.config.highlight = objUtil.merge({}, hexo.config.highlight, {
       enable     : true,
       hljs       : true,
-      line_number: false,
       wrap       : false,
-      auto_detect: true
+      auto_detect: true,
+      line_number: config.code.highlight.line_number || false
     });
 
-    if (config.code.highlight.highlightjs.bg_color) {
-      hexo.extend.filter.register('after_render:html', (html, data) => {
-        return html.replace(/(?<!<div class="hljs">)<pre.*?(class=".+?".*?)*>(.*?)<\/pre>/gis, (str, p1, p2) => {
-          if (p2.search(/mermaid/) !== -1) {
-            return str;
+    hexo.extend.filter.register('after_post_render', (page) => {
+      if (config.code.highlight.highlightjs.bg_color) {
+        page.content = page.content.replace(/(?<!<div class="hljs code-wrapper">)(<pre.+?<\/pre>)/gims, (str, p1) => {
+          if (/<code[^>]+?mermaid[^>]+?>/ims.test(p1)) {
+            return str.replace(/(class=".*?)hljs(.*?")/gims, '$1$2');
           }
-          return `<div class="hljs"><pre${p1 ? p1.replace('class="', ' class="hljs ') : ''}>${p2}</pre></div>`;
+          return `<div class="hljs code-wrapper">${p1}</div>`;
         });
-      });
-    }
+        page.content = page.content.replace(/<td class="gutter/gims, '<td class="gutter hljs');
+      } else if (!hexo.config.highlight.line_number) {
+        page.content = page.content.replace(/(?<!<div class="code-wrapper">)(<pre.+?<\/pre>)/gims, (str, p1) => {
+          if (/<code[^>]+?mermaid[^>]+?>/ims.test(p1)) {
+            return str.replace(/(class=".*?)hljs(.*?")/gims, '$1$2');
+          }
+          return `<div class="code-wrapper">${p1}</div>`;
+        });
+      }
+
+      if (hexo.config.highlight.line_number) {
+        // Mermaid block adaptation
+        page.content = page.content.replace(/<figure.+?<td class="code">.*?(<pre.+?<\/pre>).+?<\/figure>/gims, (str, p1) => {
+          if (/<code[^>]+?mermaid[^>]+?>/ims.test(p1)) {
+            return p1.replace(/(class=".*?)hljs(.*?")/gims, '$1$2').replace(/<br>/gims, '\n');
+          }
+          return str;
+        });
+      }
+      return page;
+    });
   } else if (config.code.highlight.lib === 'prismjs') {
     // Force set hexo config
     hexo.config.highlight = objUtil.merge({}, hexo.config.highlight, {
@@ -39,7 +58,22 @@ module.exports = (hexo) => {
     hexo.config.prismjs = objUtil.merge({}, hexo.config.prismjs, {
       enable     : true,
       preprocess : config.code.highlight.prismjs.preprocess || false,
-      line_number: config.code.highlight.prismjs.line_number || false
+      line_number: config.code.highlight.line_number || false
+    });
+
+    hexo.extend.filter.register('after_post_render', (page) => {
+      page.content = page.content.replace(/(?<!<div class="code-wrapper">)(<pre.+?<\/pre>)/gims, (str, p1) => {
+        if (/<code[^>]+?mermaid[^>]+?>/ims.test(p1)) {
+          str = str.replace(/<pre[^>]*?>/gims, '<pre>')
+            .replace(/(class=".*?)language-mermaid(.*?")/gims, '$1mermaid$2');
+          if (hexo.config.highlight.line_number) {
+            str = str.replace(/<span.+?line-numbers-rows.+?>.+?<\/span><\/code>/, '</code>');
+          }
+          return str;
+        }
+        return `<div class="code-wrapper">${p1}</div>`;
+      });
+      return page;
     });
   }
 };
--- themes/fluid/scripts/events/lib/lazyload.js
+++ themes/fluid/scripts/events/lib/lazyload.js
@@ -3,13 +3,13 @@
 const joinPath = require('../../utils/join-path');
 
 module.exports = (hexo) => {
-  var config = hexo.theme.config;
+  const config = hexo.theme.config;
   let loadingImage = joinPath(joinPath(hexo.config.root, config.static_prefix.internal_img), 'loading.gif');
   if (!config.lazyload || !config.lazyload.enable || !loadingImage) {
     return;
   }
   if (config.lazyload.onlypost) {
-    hexo.extend.filter.register('after_post_render', function(page) {
+    hexo.extend.filter.register('after_post_render', (page) => {
       if (page.layout !== 'post' && !page.lazyload) {
         return;
       }
@@ -19,17 +19,17 @@ module.exports = (hexo) => {
       return page;
     });
   } else {
-    hexo.extend.filter.register('after_render:html', function(str, data) {
-      if (data.page.lazyload !== false) {
-        return lazyProcess(str, loadingImage);
+    hexo.extend.filter.register('after_render:html', (html, data) => {
+      if (!data.page || data.page.lazyload !== false) {
+        return lazyProcess(html, loadingImage);
       }
     });
   }
 };
 
 const lazyProcess = (htmlContent, loadingImage) => {
-  return htmlContent.replace(/<img[^>]+src="(.*?)"[^>]*>/gi, (str, p1) => {
-    if (/srcset=/gi.test(str)) {
+  return htmlContent.replace(/<img[^>]+?src="(.*?)"[^>]*?>/gims, (str, p1) => {
+    if (/srcset=/i.test(str)) {
       return str;
     }
     return str.replace(p1, `${p1}" srcset="${loadingImage}`);
